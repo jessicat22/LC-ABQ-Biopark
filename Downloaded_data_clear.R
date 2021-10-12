bf8392369e096e1ef199b10a825086aadd7a6501
mereclay <redlistplant@bioparksociety.org>
2021-10-12 13:48:49 -0600

    Create Downloaded_data_clear.R
    Added data purge functions to remove downloaded data.

---
+++ Downloaded_data_clear.R
@@ -0,0 +1,39 @@
+#### Mapping Functions ####
+## Version 3.1
+# Started: 23 July 2021
+# Last worked on: 23 July 2021
+# Author: Clay Meredith
+# File: Downloaded_data_clear.R
+# Description: Removes all downloaded data (geospatial data, and
+#              species occurrence data) from Back_end folder.
+
+
+#### Set Working Directory ####
+if (!exists("current.dir")){
+  current.dir <- dirname(parent.frame(2)$ofile)
+  setwd(current.dir)
+  print(getwd())
+}
+
+# Define file remover function
+remove_files <- function (x){
+  unlink(x, recursive = TRUE, force = TRUE)
+}
+
+#### Remove Geospatial Data ####
+# Define files to remove
+files_to_remove <- c("Back_end/Dependencies/Geospatial_data/hotspot",
+                     "Back_end/Dependencies/Geospatial_data/realm",
+                     "Back_end/Dependencies/Geospatial_data/wgsrpd-master")
+# Remove files
+lapply(files_to_remove, remove_files)
+
+#### Remove Downloaded GBIF Data ####
+# Define files to remove
+files_to_remove <- 
+  list.files(path = "Back_end/Downloaded_Datasets/", 
+             include.dirs = TRUE,
+             full.names = TRUE)
+
+# Remove files
+lapply(files_to_remove, remove_files)
